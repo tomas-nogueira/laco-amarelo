151eaae49d30ce26a06152cb45c0e7748b2fcca7
miguelmnogueira <miguelmiguelmnogueira@gmail.com>
2024-02-02 14:34:50 -0300

    About Me Polido e Finalizado com imagem Placeholder

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import Header from "../components/Header";
 import Style from "../styles/aboutme.module.css";
 import BannerPhoto from "../assets/aboutmebanner.jpg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import ReactDOM from "react-dom";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaVimeoV } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 function AboutMe() {
     return (
@@ -11,35 +15,63 @@ function AboutMe() {
             <Header />
             <div className={Style.container}>
                 <div className={Style.imageContainer}>
-                    <img src={BannerPhoto} alt="Imagem" />
+                    <img
+                        src="https://i.ibb.co/DwfyVK4/Vector.png"
+                        alt="Imagem"
+                    />
                 </div>
-                <div className={Style.textContainer}>
-                    <div className={Style.title}>
-                        <h2>
-                            Olá! Eu sou a <span>Carla</span>
-                        </h2>
-                    </div>
-                    <div className={Style.paragraph}>
-                        <p>
-                            Em constante busca por uma fotografia sem padrões,
-                            poses ou regras. Um dia livre para amar, cuidar,
-                            brincar e se encantar com os sentimentos mais
-                            simples e mais importante da sua família.
-                            <br />
-                            <br />É perceber que o simples fato de estarem
-                            juntos é o momento mais especial que existe e a
-                            memória mais verdadeira que eu poderia entregar para
-                            vocês. Permita-se amar e se reconectar
-                            <br />
-                            <br /> Nos vemos em breve! Com carinho e sempre,
-                            Carla.
-                        </p>
+                <div className={Style.textMainContainer}>
+                    <div className={Style.textContainer}>
+                        <div className={Style.title}>
+                            <h2>
+                                Olá! Eu sou a <span>Carla</span>
+                            </h2>
+                        </div>
+                        <div className={Style.paragraph}>
+                            <p>
+                                Em constante busca por uma fotografia sem
+                                padrões, poses ou regras. Um dia livre para
+                                amar, cuidar, brincar e se encantar com os
+                                sentimentos mais simples e mais importante da
+                                sua família.
+                                <br />
+                                <br />É perceber que o simples fato de estarem
+                                juntos é o momento mais especial que existe e a
+                                memória mais verdadeira que eu poderia entregar
+                                para vocês. Permita-se amar e se reconectar
+                                <br />
+                                <br /> Nos vemos em breve! Com carinho e sempre,
+                                Carla.
+                            </p>
+                        </div>
                     </div>
                     <div className={Style.iconContainer}>
-                        <FontAwesomeIcon icon="fa-brands fa-instagram" />
-                        <FontAwesomeIcon icon="fa-brands fa-facebook-f" />
-                        <FontAwesomeIcon icon="fa-brands fa-vimeo-v" />
-                        <FontAwesomeIcon icon="fa-brands fa-whatsapp" />
+                        <a
+                            target="_blank"
+                            href="https://www.instagram.com/laco.amarelo"
+                        >
+                            <FaInstagram className={Style.icon} color="white" />
+                        </a>
+                        <a
+                            target="_blank"
+                            href="https://www.facebook.com/lacoamarelofotografia"
+                        >
+                            <FaFacebookF
+                                className={(Style.icon, Style.facebook)}
+                            />
+                        </a>
+                        <a
+                            target="_blank"
+                            href="https://vimeo.com/lacoamarelofotografia"
+                        >
+                            <FaVimeoV className={Style.icon} />
+                        </a>
+                        <a
+                            target="_blank"
+                            href="https://web.whatsapp.com/send?phone=5514988059383"
+                        >
+                            <FaWhatsapp className={Style.icon} />
+                        </a>
                     </div>
                 </div>
             </div>
