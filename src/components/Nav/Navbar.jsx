728308485a1ceb6a63513190d41cec6df79ce6b5
miguelmnogueira <miguelmiguelmnogueira@gmail.com>
2024-02-07 21:55:21 -0300

    Header polido

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,39 +1,39 @@
-import React from 'react';
-import styled from 'styled-components';
-import Burger from '../Nav/Burguer';
+import React from "react";
+import styled from "styled-components";
+import Burger from "../Nav/Burguer";
 
 const Nav = styled.nav`
-  width: 100%;
-  height: 80px;
-  border-bottom: 1px solid #f1f1f1;
-  padding: 0 20px;
-  display: flex;
-  justify-content: space-between;
-  align-itens: center;
+    width: 100%;
+    height: 80px;
+    padding: 0 20px;
+    display: flex;
+    justify-content: space-between;
+    align-itens: center;
 
-  .logo {
-    padding: 15px 0;
-  }
+    .logo {
+        padding: 15px 0;
+    }
 
-  .logoS {
-    width: 250px;
-  }
-`
+    .logoS {
+        width: 270px;
+    }
+`;
 
 const Navbar = () => {
-  return (
-    <Nav>
-      <div className="logo">
-        <a href="/">
-          <img className='logoS'
-              src="https://cdn-sites-images.46graus.com/files/photos/bc1139b2/2c0773e3-96a2-4afc-ad65-bfe4cfaa55bc/logo_oficial_2-354x96.jpg"
-              alt="Logo Laço Amarelo"
-          />
-        </a>
-      </div>
-      <Burger />
-    </Nav>
-  )
-}
+    return (
+        <Nav>
+            <div className="logo">
+                <a href="/">
+                    <img
+                        className="logoS"
+                        src="https://cdn-sites-images.46graus.com/files/photos/bc1139b2/2c0773e3-96a2-4afc-ad65-bfe4cfaa55bc/logo_oficial_2-354x96.jpg"
+                        alt="Logo Laço Amarelo"
+                    />
+                </a>
+            </div>
+            <Burger />
+        </Nav>
+    );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/Nav/RightNav.jsx
+++ src/components/Nav/RightNav.jsx
@@ -1,54 +1,62 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import Style from "./rightnav.module.css";
 
 const Ul = styled.ul`
-  list-style: none;
-  display: flex;
-  flex-flow: row nowrap;
-
-  li {
-    padding: 30px 18px;
-    font-size: 1.1em;
-    cursor: pointer;
-  }
-
-  .ul{
-    align-itens:center;
-    display:flex;
-    justify-content: center;
-  }
-
-
-  @media (max-width: 840px) {
-    flex-flow: column nowrap;
-    background-color: #FFD83D;
-    position: fixed;
-    transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
-    top: 0;
-    right: 0;
-    height: 100vh;
-    width: 300px;
-    padding-top: 3.5rem;
-    transition: transform 0.3s ease-in-out;
+    list-style: none;
+    display: flex;
+    flex-flow: row nowrap;
 
     li {
-      color: #fff;
+        font-size: 1.1em;
+        cursor: pointer;
     }
-  }
-  li:hover {
-    background-color: rgb(255, 239, 175);
-  }
+
+    .ul {
+        align-itens: center;
+        display: flex;
+        justify-content: center;
+    }
+
+    @media (max-width: 840px) {
+        flex-flow: column nowrap;
+        background-color: #ffd83d;
+        position: fixed;
+        transform: ${({ open }) =>
+            open ? "translateX(0)" : "translateX(100%)"};
+        top: 0;
+        right: 0;
+        height: 100vh;
+        width: 300px;
+        padding-top: 3.5rem;
+        transition: transform 0.3s ease-in-out;
+
+        li {
+            color: #fff;
+        }
+    }
+    li:hover {
+        background-color: rgb(255, 239, 175);
+    }
 `;
 
 const RightNav = ({ open }) => {
-  return (
-    <Ul open={open} className='ul'>
-      <li>GALERIA</li>
-      <li><a href="/produtos">PRODUTOS</a></li>
-      <li><a href="/sobremim">SOBRE MIM</a></li>
-      <li><a href="/contato">CONTATO</a></li>
-    </Ul>
-  )
-}
+    return (
+        <Ul open={open} className={Style.ul}>
+            <li className={Style.li}>
+                <a href="/galeria">GALERIA</a>
+            </li>
+            <li className={Style.li}>
+                <a href="/produtos">PRODUTOS</a>
+            </li>
+            <li className={Style.li}>
+                <a href="/sobremim">SOBRE MIM</a>
+            </li>
+            <li className={Style.li}>
+                <a href="/contato">CONTATO</a>
+            </li>
+        </Ul>
+    );
+};
 
-export default RightNav
+export default RightNav;
